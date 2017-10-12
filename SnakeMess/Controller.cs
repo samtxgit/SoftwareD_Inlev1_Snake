44c4bb20a410f19a96b0ceeea8fdb25cf0f1e614
william <william.judi@gmail.com>
2017-10-12 13:34:54 +0200

    done

--- SnakeMess/Controller.cs
+++ SnakeMess/Controller.cs
@@ -6,19 +6,42 @@ using System.Diagnostics;
 
 namespace SnakeMess
 {
+    enum Direction { up, right, down, left };
+
     class Controller
     {
-        enum Direction { up , right, down, left };
-        static bool gameOver = false;
-        static bool pause = false;
-        static bool inUse = false;
-        static Direction newDirection;
-        static Direction lastDirection;
+        #region Static fields
+        private static bool GameOver = false;
+        private static bool pause = false;
+        private static bool InUse = false;
+        private static Direction NewDirection;
+        private static Direction LastDirection;
+        #endregion
 
+        #region Static method
+        public static void HandleUserInput()
+        {
+            ConsoleKeyInfo cki = ReadKey(true);
+            if (cki.Key == ConsoleKey.Escape)
+                GameOver = true;
+            else if (cki.Key == ConsoleKey.Spacebar)
+                pause = !pause;
+            else if (cki.Key == ConsoleKey.UpArrow && LastDirection != Direction.down)
+                NewDirection = Direction.up;
+            else if (cki.Key == ConsoleKey.RightArrow && LastDirection != Direction.left)
+                NewDirection = Direction.right;
+            else if (cki.Key == ConsoleKey.DownArrow && LastDirection != Direction.up)
+                NewDirection = Direction.down;
+            else if (cki.Key == ConsoleKey.LeftArrow && LastDirection != Direction.right)
+                NewDirection = Direction.left;
+        }
+        #endregion
+
+        #region Main()
         public static void Main(string[] arguments)
         {
-            newDirection = Direction.down;
-            lastDirection = newDirection;
+            NewDirection = Direction.down;
+            LastDirection = NewDirection;
             int boardW = WindowWidth;
             int boardH = WindowHeight;
             CursorVisible = false;
@@ -28,11 +51,11 @@ namespace SnakeMess
 
             Stopwatch t = new Stopwatch();
             t.Start();
-            while (!gameOver)
+            while (!GameOver)
             {
                 if (KeyAvailable)
                 {
-                    handleUserInput();
+                    HandleUserInput();
                 }
                 if (!pause)
                 {
@@ -42,7 +65,7 @@ namespace SnakeMess
                     Point tail = new Point(gameObject.GetSnakeBodysFirst());
                     Point head = new Point(gameObject.GetSnakeBodysLast());
                     Point newH = new Point(head);
-                    switch (newDirection)
+                    switch (NewDirection)
                     {
                         case Direction.up:
                             newH.Y -= 1;
@@ -58,36 +81,36 @@ namespace SnakeMess
                             break;
                     }
                     if (newH.X < 0 || newH.X >= boardW)
-                        gameOver = true;
+                        GameOver = true;
                     else if (newH.Y < 0 || newH.Y >= boardH)
-                        gameOver = true;
+                        GameOver = true;
                     if (newH.X == gameObject.GetApplePoint_X() && newH.Y == gameObject.GetApplePoint_Y())
                     {
                         if (gameObject.GetSnakeCount() + 1 >= boardW * boardH)
                             // No more room to place apples - game over.
-                            gameOver = true;
+                            GameOver = true;
                         else
                         {
-                            inUse = gameObject.PrintNextApple();
+                            InUse = gameObject.PrintNextApple();
                         }
                     }
-                    if (!inUse)
+                    if (!InUse)
                     {
                         gameObject.RemoveitemFromSnakeAtIndex(0);
                         foreach (Point x in gameObject.GetSnake().bodyList)
                             if (x.X == newH.X && x.Y == newH.Y)
                             {
                                 // Death by accidental self-cannibalism.
-                                gameOver = true;
+                                GameOver = true;
                                 break;
                             }
                     }
-                    if (!gameOver)
+                    if (!GameOver)
                     {
                         ForegroundColor = Yellow;
                         SetCursorPosition(head.X, head.Y);
                         Write("0");
-                        if (!inUse)
+                        if (!InUse)
                         {
                             SetCursorPosition(tail.X, tail.Y);
                             Write(" ");
@@ -97,32 +120,17 @@ namespace SnakeMess
                             ForegroundColor = Green;
                             SetCursorPosition(gameObject.GetApplePoint_X(), gameObject.GetApplePoint_Y());
                             Write("$");
-                            inUse = false;
+                            InUse = false;
                         }
                         gameObject.AddToSnake(newH);
                         ForegroundColor = Yellow;
                         SetCursorPosition(newH.X, newH.Y);
                         Write("@");
-                        lastDirection = newDirection;
+                        LastDirection = NewDirection;
                     }
                 }
             }
         }
-
-        public static void handleUserInput() {
-            ConsoleKeyInfo cki = ReadKey(true);
-            if (cki.Key == ConsoleKey.Escape)
-                gameOver = true;
-            else if (cki.Key == ConsoleKey.Spacebar)
-                pause = !pause;
-            else if (cki.Key == ConsoleKey.UpArrow && lastDirection != Direction.down)
-                newDirection = Direction.up;
-            else if (cki.Key == ConsoleKey.RightArrow && lastDirection != Direction.left)
-                newDirection = Direction.right;
-            else if (cki.Key == ConsoleKey.DownArrow && lastDirection != Direction.up)
-                newDirection = Direction.down;
-            else if (cki.Key == ConsoleKey.LeftArrow && lastDirection != Direction.right)
-                newDirection = Direction.left;
-        }
+        #endregion
     }
 }
